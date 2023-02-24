674aeef2bb5533de94969819984d55fc443bed97
reuben <reuben.vincent.reid@gmail.com>
2023-02-24 11:36:07 +0000

    us

--- components/ContactLanding.tsx
+++ components/ContactLanding.tsx
@@ -4,7 +4,7 @@ export const ContactLanding = () => {
       <div>
         <div className="flex">
           <div className="m-auto mt-[20%] lg:mt-[10%] flex flex-col text-center">
-            <div className="w-[70vw] lg:w-[50vw] tracking-[0.05em] lg:text-[30px] text-[20px] mt-6">Please contact me at reuben.vincent.reid@gmail.com, or phone
+            <div className="w-[70vw] lg:w-[50vw] tracking-[0.05em] lg:text-[30px] text-[20px] mt-6">Please contact us at reuben.vincent.reid@gmail.com, or phone
               me on 07477937515 if you need any help or have any questions. </div>
           </div>
         </div>
