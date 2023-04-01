cd977f3cca72ebfcb6eb8a7612202171da677316
reuben <reuben.vincent.reid@gmail.com>
2023-04-01 20:39:13 +0100

    updating contact + adding registry

--- components/ContactLanding.tsx
+++ components/ContactLanding.tsx
@@ -4,8 +4,15 @@ export const ContactLanding = () => {
       <div>
         <div className="flex">
           <div className="m-auto mt-[20%] lg:mt-[10%] flex flex-col text-center">
-            <div className="w-[70vw] lg:w-[50vw] tracking-[0.05em] lg:text-[30px] text-[20px] mt-6">Please contact us at reuben.vincent.reid@gmail.com, or phone
-              us on 07477937515 if you need any help or have any questions. </div>
+            <div className="w-[70vw] lg:w-[50vw] tracking-[0.05em] lg:text-[30px] text-[20px] mt-6">
+              Please contact us at reuben.vincent.reid@gmail.com and grace.eakin@yahoo.co.uk,
+              or phone Reuben on 07477937515 and Grace on 07814838080 if you need any help or have any questions. </div>
+            <div className="w-[70vw] lg:w-[50vw] tracking-[0.05em] lg:text-[30px] text-[20px] mt-6">
+              You can also access our registry&nbsp;
+              <a className="underline" target="_blank" href="https://www.johnlewis.com/wish-list/MNFMVQD" rel="noopener noreferrer">
+                here.
+              </a>
+            </div>
           </div>
         </div>
       </div>
